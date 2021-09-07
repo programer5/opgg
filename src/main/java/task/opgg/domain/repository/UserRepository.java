a1961d572f112acb7110ba98a5b53d26c3930253
minseo23 <neverdie4757@gmail.com>
2021-09-07 21:47:42 +0900

    수정

--- src/main/java/task/opgg/domain/repository/UserRepository.java
+++ src/main/java/task/opgg/domain/repository/UserRepository.java
@@ -3,9 +3,11 @@ package task.opgg.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import task.opgg.domain.dto.UserResponseDto;
 import task.opgg.domain.entity.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("select new task.opgg.domain.dto.UserResponseDto(u.id, u.username, u.rank) from User u where u.username = :username")
